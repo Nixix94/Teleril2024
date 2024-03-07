5aa367583f13cd5392c3eef89251817ce70fddae
belle.enrico <100956965+Nixix94@users.noreply.github.com>
2024-03-07 17:29:23 +0100

    Create 02_install.packages.R

---
+++ Code/02_install.packages.R
@@ -0,0 +1,6 @@
+# Installazione nuovi pacchetti in R
+
+install.packages("terra")
+# importante usare "", solitamente impostata di default il sito del CRAN
+
+# dopo averlo installato il pacchetto deve esser richiamato attraverso la funzione library
