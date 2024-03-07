d16e1cb4c2e7af6ad5cce2f5340036c72502c6c3
belle.enrico <100956965+Nixix94@users.noreply.github.com>
2024-03-07 17:05:03 +0100

    Create 01_beginning.R

---
+++ Code/01_beginning.R
@@ -0,0 +1,26 @@
+# Hello World
+
+# Nota molto importante per l'esame saranno i commenti 
+# Ctrl+Maiusc+C vedere il codice che struttura una qualsiasi pagina web
+a <- 6*6
+b <- 7*7
+a+b
+# prime lezioni
+# array - vettori di dati - serie di dati concatenati insieme 
+
+flowers <- 3, 4, 6, 8, 15, 20
+# fiori per specie in una certa area
+# funzione c() me serve per concatenare i dati, altrimenti nell'esempio sopra mi avrebbe letto solo la prima cifra
+
+flowers <- c(3, 4, 6, 8, 15, 20)
+flowers
+
+insects <- c(10, 16, 24, 32, 45, 60)
+
+# metto in relazione i dati
+# funzione plot()
+plot(flowers, insects)
+# pch=10 point character decido che tipo di simboloo ritrovo nel plot
+# cex=2 invece è per decidere la grandezza del punto (0.5 per più piccolo), si usa . anzichè , per i decimali
+# clo= invece per il colore 
+
