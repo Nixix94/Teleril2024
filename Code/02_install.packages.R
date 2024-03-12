0d62e0b97ecc08851a3502697e80f0c5e899440b
belle.enrico <100956965+Nixix94@users.noreply.github.com>
2024-03-12 17:26:35 +0100

    Update 02_install.packages.R

--- Code/02_install.packages.R
+++ Code/02_install.packages.R
@@ -2,16 +2,16 @@
 
 install.packages("terra")
 # importante usare "", solitamente impostata di default il sito del CRAN
-
+library(terra)
 # dopo averlo installato il pacchetto deve esser richiamato attraverso la funzione library
 
 # pacchetto imageRy pacchetto creato su misura per agevolare la didattica, presente all'interno di Github
 # funzione "devtools" mi permette di usare la funzione per scaricare i pacchetti da Github
-# install.github
+# install_github
 # imageRy
 
 # per indicare da dove arriva una funzione metto pacchetto di provenienza :: funzione
-devtools::install.packages("devtools")
-install.github("imageRy")
-
+install.packages("devtools")
+devtools::install_github("ducciorocchini/imageRy")
+library(imageRy)
 # i pachetti che si usano all'interno del codice vanno messi tutti in alto!!!
