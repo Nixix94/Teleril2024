e1f5816f8f8068d160a86930f33aafcbb4d0bf77
belle.enrico <100956965+Nixix94@users.noreply.github.com>
2024-03-14 16:44:07 +0100

    Update 03_visualisation.R

--- Code/03_visualisation.R
+++ Code/03_visualisation.R
@@ -9,3 +9,17 @@ im.list()
 #importiamo i dati di noto interesse: matogrosso
 #oggetto assegnazione funzione argomento
 mato <- im.import("matogrosso_ast_2006209_lrg.jpg")
+
+# Plotting the data
+plot(mato)
+
+#importo altre immagini per andar in ordine e visualizzare prima un esempio con le singole bande
+
+b2 <- im.import("sentinel.dolomites.b2.tif")
+# immagine satellitare del programma Sentinel composta da varie bande, con il valore 2 si intende banda blu
+
+# andiamo a cambiare la scala di colori attraverso la funzione colorRampPalette
+clg <- colorRampPalette(c("black", "grey", "little grey"))(3)
+# uso c (concatenate) per raccogliere i colori
+# 3 sono le gradazione che scelgo, in questa funzione questo argomento va messo fuori dalle parentesi
+# do un nome così sarà più facile da riprendere
